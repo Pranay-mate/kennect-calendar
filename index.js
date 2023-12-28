b59fe0199a9a8962961a522bda9cc815fddf066f
LAPTOP-747E5JBF\matep <matepranay11@gmail.com>
2023-12-28 14:53:48 +0530

    FIX: web & Api issue

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 const PORT = 3000;
 const app = express();
 app.use(express.static('./'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/calculateDate', (req, res) => {
   try {
